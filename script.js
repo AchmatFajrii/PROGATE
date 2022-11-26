56a590680b8a1eab2ce4eb3654d66157f2185265
Achmat Fajri <achmatfajri@gmail.com>
2022-11-25 19:46:06 -0800

    nambah video dan darkmode

--- script.js
+++ script.js
@@ -1,30 +1,82 @@
-const darkButton = document.getElementById('dark');
-const textDark = document.querySelector('.textdark');
-const headingDark4 = document.querySelector('h4');
-const headingDark3 = document.querySelector('h3');
-const headingDarkID3 = document.querySelector('#h3');
-const paragraf = document.querySelector('p');
-const textEmail = document.querySelector('#email');
-const textMessage = document.querySelector('#message');
-const p1 = document.querySelector('#p1');
-const p2 = document.querySelector('#p2');
-const p3 = document.querySelector('#p3');
+// const darkButton = document.getElementById('dark');
+// const textDark = document.querySelector('.textdark');
+// const headingDark4 = document.querySelector('h4');
+// const headingDark3 = document.querySelector('h3');
+// const headingDarkID3 = document.querySelector('#h3');
+// const paragraf = document.querySelector('p');
+// const textEmail = document.querySelector('#email');
+// const textMessage = document.querySelector('#message');
+// const p1 = document.querySelector('#p1');
+// const p2 = document.querySelector('#p2');
+// const p3 = document.querySelector('#p3');
 
 
-darkButton.addEventListener('click', function () {
-    document.body.style.backgroundColor = '#292929';
-    darkButton.innerHTML = 'Light Mode';
-    darkButton.style.backgroundColor = 'white';
-    darkButton.style.color = '#292929';
-    textDark.style.color = 'white';
-    headingDark4.style.color = 'white';
-    headingDark3.style.color = 'white';
-    headingDarkID3.style.color = 'white';
-    paragraf.style.color = 'white';
-    textEmail.style.color = 'white';
-    textMessage.style.color = 'white';
-    p1.style.color = 'white';
-    p2.style.color = 'white';
-    p3.style.color = 'white';
-    alert('refresh kembali untuk tampilan light mode');
-})
+// darkButton.addEventListener('click', function () {
+//     document.body.style.backgroundColor = '#292929';
+//     darkButton.innerHTML = 'Light Mode';
+//     darkButton.style.backgroundColor = 'white';
+//     darkButton.style.color = '#292929';
+//     textDark.style.color = 'white';
+//     headingDark4.style.color = 'white';
+//     headingDark3.style.color = 'white';
+//     headingDarkID3.style.color = 'white';
+//     paragraf.style.color = 'white';
+//     textEmail.style.color = 'white';
+//     textMessage.style.color = 'white';
+//     p1.style.color = 'white';
+//     p2.style.color = 'white';
+//     p3.style.color = 'white';
+//     alert('refresh kembali untuk tampilan light mode');
+// })
+
+const tChange = document.querySelector('button.change');
+const title = document.querySelector('.title');
+const title2 = document.querySelector('.title h3');
+const title3 = document.querySelector('.title h4');
+const paragraf = document.querySelector('.title p');
+const tImage1 = document.querySelector('.image p');
+const tImage2 = document.querySelector('.image2 p');
+const tImage3 = document.querySelector('.image3 p');
+const title4 = document.querySelector('h3.contact');
+const labelEmail = document.querySelector('label#email');
+const labelMessage = document.querySelector('label#message');
+
+tChange.addEventListener('click', function() {
+    document.body.classList.toggle('light-dark-mode');
+    title.classList.toggle('text-white');
+    title2.classList.toggle('text-white');
+    title3.classList.toggle('text-white');
+    title4.classList.toggle('text-white');
+    paragraf.classList.toggle('text-white');
+    tImage1.classList.toggle('text-white');
+    tImage2.classList.toggle('text-white');
+    tImage3.classList.toggle('text-white');
+    labelEmail.classList.toggle('text-white');
+    labelMessage.classList.toggle('text-white');
+});
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+scrollFunction();
+};
+
+function scrollFunction() {
+if (
+document.body.scrollTop > 20 ||
+document.documentElement.scrollTop > 20
+) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
