3fb77572bc4a5d3f08109e34dd1ceb2952bd7257
Achmat Fajri <achmatfajri@gmail.com>
2022-12-05 07:14:29 -0800

    memperbaiki card dan menambahkan animasi slide

--- script.js
+++ script.js
@@ -108,3 +108,20 @@ const modal = document.getElementById("myModal");
     closeIcon.addEventListener('click', () => {
         modal.style.display = "none";
     });
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
